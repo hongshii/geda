082648cc2901180a3ebd1674372dfdd8ed16e64d
hongshii <ghdduwjd123@naver.com>
2022-07-12 12:31:34 +0900

    7.12수정

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ new fullpage('#fullpage', {
 	autoScrolling:true,
 	scrollHorizontally: true,
     scrollBar:true,
-    sectionsColor : ['#ccc', '#fff', '#ccc', '#fff', '#ccc'],
     navigation: true,
 	navigationPosition: 'right',
 	navigationTooltips: ['GAH소개', '제다큐어','연구개발', '고객지원', '판매병원 찾기'],
